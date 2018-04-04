de44618b283f4ecbe65b76cd3ff41cc2a2582d66
Yue Zhang <yjzhang@cs.washington.edu>
2018-04-03 19:57:25 -0700

    dependencies

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
 from setuptools import setup, find_packages
 #from distutils.extension import Extension
 #from Cython.Build import cythonize
-import numpy
 
 
 #extensions = [
 #]
+
+install_requires = [
+        'nmslib',
+        'numpy'
+        ]
 
 
 setup(
@@ -15,9 +19,9 @@ setup(
     author_email='yjzhang@cs.washington.edu',
     url='https://github.com/yjzhang/simplex_sample',
     license='MIT',
-    include_dirs=[numpy.get_include()],
+    install_requires=install_requires,
 #    ext_modules = cythonize(extensions),
     packages=find_packages("."),
     test_suite='nose.collector',
-    tests_require=['nose', 'flaky'],
+    tests_require=['nose'],
 )
